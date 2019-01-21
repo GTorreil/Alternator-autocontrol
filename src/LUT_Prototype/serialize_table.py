a9d250a63dd632e4bcf72f73b81abf7164a22986
uiguig <guillaumetorreilles@yahoo.fr>
2019-01-21 22:32:12 +0100

    Create basic map serialiser

--- src/LUT_Prototype/serialize_table.py
+++ src/LUT_Prototype/serialize_table.py
@@ -1,1 +1,19 @@
+import csv
+import pyperclip
 
+filename = "map_inter_i32.csv"
+
+serialized = "map32,"
+
+with open(filename, 'r') as csvfile:
+     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     for row in reader:
+         row = [int(numeric_string) for numeric_string in row]
+         serialized += str(row)[1:-1].strip()+",l,"
+
+serialized = serialized[0:-2] + "end"
+
+#print(serialized)
+pyperclip.copy(serialized)
+
+print("Serialized map copied to the clipboard")
