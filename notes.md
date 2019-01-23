e5b4b0e55207b9eb186e5432d4a263a67ea8212b
uiguig <guillaumetorreilles@yahoo.fr>
2019-01-23 08:19:05 +0100

    Update map serialiser to match embedded decoder

---
+++ notes.md
@@ -0,0 +1,3 @@
+## Justes qq notes pour le développement 
+
+couplage du stator en double étoile
--- src/LUT_Prototype/serialize_table.py
+++ src/LUT_Prototype/serialize_table.py
@@ -3,15 +3,15 @@ import pyperclip
 
 filename = "map_inter_i32.csv"
 
-serialized = "map32,"
+serialized = "map32\n"
 
 with open(filename, 'r') as csvfile:
      reader = csv.reader(csvfile, delimiter=',', quotechar='|')
      for row in reader:
          row = [int(numeric_string) for numeric_string in row]
-         serialized += str(row)[1:-1].strip()+",l,"
+         serialized += str(row)[1:-1].strip().replace(" ", "")+",\n"
 
-serialized = serialized[0:-2] + "end"
+serialized = serialized + "end"
 
 #print(serialized)
 pyperclip.copy(serialized)
